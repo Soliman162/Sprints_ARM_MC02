851acadcce5376106a5bd34d541df8b98fcb18eb
Soliman162 <ahmedsliman215@gmail.com>
2022-07-11 16:45:54 +0200

    SCB_Init

--- Src/Mcal/SCB.c
+++ Src/Mcal/SCB.c
@@ -2,7 +2,7 @@
 /**********************************************************************************************************************
  *  FILE DESCRIPTION
  *  -----------------------------------------------------------------------------------------------------------------*/
-/**        \file  IntCrtl.c
+/**        \file  SCB.c
  *        \brief  Nested Vector Interrupt Controller Driver
  *
  *      \details  The Driver Configure All MCU interrupts Priority into gorups and subgroups
@@ -14,10 +14,13 @@
  *  INCLUDES
  *********************************************************************************************************************/
 #include "std_types.h"
+#include "BIT_MATH.h"
 #include "MCU_HW.h"
-#include "IntCtrl_types.h"
-#include "IntCtrl.h"
-#include "IntCtrl_Cfg.h"
+
+#include "SCB_types.h"
+#include "SCB_Cfg.h"
+#include "SCB.h"
+
 /**********************************************************************************************************************
 *  LOCAL MACROS CONSTANT\FUNCTION
 *********************************************************************************************************************/	
@@ -44,9 +47,9 @@
 
 
 /******************************************************************************
-* \Syntax          : void IntCrtl_Init(void)                                      
-* \Description     : initialize Nvic\SCB Module by parsing the Configuration 
-*                    into Nvic\SCB registers                                    
+* \Syntax          : void SCB_Init(void)                                      
+* \Description     : initialize SCB\SCB Module by parsing the Configuration 
+*                    into SYS_Ctrl registers                                    
 *                                                                             
 * \Sync\Async      : Synchronous                                               
 * \Reentrancy      : Non Reentrant                                             
@@ -54,56 +57,63 @@
 * \Parameters (out): None                                                      
 * \Return value:   : None
 *******************************************************************************/
-
-void Init_voidIntCtrl(void)
+void Init_voidSCB(void)
 {
-    u8 LOC_i_u8;
-    u8 LOC_j_u8;
-    u8 LOC_N_u8;
+    #if WAKE_UP_ON_PENDING == ENABLE
+        SCB_REGs->SYSCTRL |= 16;
+    #endif
 
-    /* set group_priorty / subgroub priority APINT in SCB*/
-    SCB_REGs->APINT |= (0x05FA0000|(GROUP_PRIORITY_X_SUBPRIORITY_X<<8));
+    #if SLEEP_ONISR_EXIT == ENABLE
+        SCB_REGs->SYSCTRL |= 2;
+    #endif
+    #if STACK_ALIGNMENT_ON_EXCEPTION_ENTRY == STACK_8_BYTE_ALIGNED
+        SCB_REGs->CFGCTRL |= 1<<9;
+    #endif
+    #if TRAP_DIV_BY_0 == ENABLE
+        SCB_REGs->CFGCTRL |= 1<<4;
+    #endif
 
-    /* set group_priorty / subgroub priority in NVIC */
-    for(LOC_i_u8=0;LOC_i_u8<MAX_INTRRUPT_NUMBER;LOC_i_u8++)
-    {
-        LOC_N_u8 = (Intrrupt_u8SET[LOC_i_u8][0])/4;
-        LOC_j_u8 = (Intrrupt_u8SET[LOC_i_u8][0]) - 4*LOC_N_u8;
-
-        PRI_REGS->PRI[LOC_N_u8] |= (Intrrupt_u8SET[LOC_i_u8][1]<<(LOC_j_u8*8+5)) ;
-    }
-    
-    /* Enable/disable IRQ */
-    for(LOC_i_u8=0;LOC_i_u8<MAX_INTRRUPT_NUMBER;LOC_i_u8++)
-    {
-        LOC_N_u8 = Intrrupt_u8SET[LOC_i_u8][0]/32;
-        LOC_j_u8 = (Intrrupt_u8SET[LOC_i_u8][0])%32;
-        EN_REGs->EN[LOC_N_u8] |= 1<<( LOC_j_u8 ) ;
-    }
-
+    #if TRAP_ON_UNALIGNED_ACCESS == ENABLE
+        SCB_REGs->CFGCTRL |= 1<<3 ;
+    #endif
 }
 
+void Enable_voidFault(u8 Copy_u8Fault)
+{
+    SCB_REGs->SYSHNDCTRL |= 1<<Copy_u8Fault;
+}
+void Disable_voidFault(u8 Copy_u8Fault)
+{
+    SCB_REGs->SYSHNDCTRL &= ~(1<<Copy_u8Fault);
+}
+void SET_voidFAULT_PENDING(u8 Copy_u8Fault)
+{
+    SCB_REGs->SYSHNDCTRL |= 1<<Copy_u8Fault;
+}
+void CLR_voidFAULT_PENDING(u8 Copy_u8Fault)
+{
+    SCB_REGs->SYSHNDCTRL &= ~(1<<Copy_u8Fault);
+}
+void SET_voidINTERRUPT_PENDING(u8 Copy_u8Eception)
+{
+    SCB_REGs->INTCTRL |= 1<<Copy_u8Eception;
+}
+void CLEAR_voidINTERRUPT_PENDING(u8 Copy_u8Eception)
+{
+    SCB_REGs->INTCTRL |= 1<<(Copy_u8Eception-1);
+}
+void RESET_voidSystem(void)
+{
+    SCB_REGs->APINT |= 4;
+}
+void DEEP_SLEEP_voidEnable(void)
+{
+    SCB_REGs->SYSCTRL |= 4;
+}
+void DEEP_SLEEP_voidDisable(void)
+{
+    SCB_REGs->SYSCTRL &= ~4;
+}
 /**********************************************************************************************************************
- *  END OF FILE: IntCrtl.c
+ *  END OF FILE: SCB.c
  *********************************************************************************************************************/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
